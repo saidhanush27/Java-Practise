d27b92e281b203028869c229e6ea34a94cab6735
Sai Dhanush VR <sai2003vr@gmail.com>
2025-03-08 13:24:26 +0530

    Add files via upload

---
+++ Symbol.java
@@ -0,0 +1,10 @@
+public class Symbol{
+    public  Symbol (String s){
+        System.out.println(s +" !&*?%$$//# ");
+     }
+     public static void main(String[] args) {
+        Symbol obj = new Symbol(" These are the symbols in java ");
+     }
+}
+
+
