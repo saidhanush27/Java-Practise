af75507c7c02c77607f75cea6eb0ae4eba91da24
Sai Dhanush VR <sai2003vr@gmail.com>
2025-05-29 14:46:12 +0530

    Delete Case.java

--- Case.java
+++
@@ -1,8 +0,0 @@
-// uppercase to Lowecase and vice versa
-public class Case{
-    public static void main(String[] args) {
-       String name ="sai";
-       System.out.println (name.toLowerCase());
-}
-}
- 
